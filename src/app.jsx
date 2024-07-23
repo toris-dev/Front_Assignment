3133ed64336cf4f889663e77f02a946200e8d8b1
toris <ironjustlikethat@gmail.com>
2024-07-23 23:50:53 +0900

    💡 [Feat] : 드래그 제약 조건 기능 추가

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { DragDropContext } from "react-beautiful-dnd";
+import toast, { Toaster } from "react-hot-toast";
 import Column from "./components/Column";
 
 export default function App() {
@@ -9,7 +10,6 @@ export default function App() {
       content: `item ${k}`,
     }));
 
-  // 각 컬럼을 개별 상태로 관리
   const [columns, setColumns] = useState({
     col1: { id: "col1", items: getItems(10) },
     col2: { id: "col2", items: getItems(10, 10) },
@@ -17,9 +17,62 @@ export default function App() {
     col4: { id: "col4", items: getItems(10, 30) },
   });
 
+  const [draggingItemId, setDraggingItemId] = useState(null);
+  const [invalidDrop, setInvalidDrop] = useState(false);
+
+  const handleDragStart = (start) => {
+    setDraggingItemId(start.draggableId);
+    setInvalidDrop(false);
+  };
+
+  const handleDragUpdate = (update) => {
+    const { destination, source } = update;
+    if (!destination) {
+      setInvalidDrop(false);
+      return;
+    }
+
+    const draggingItemK = parseInt(draggingItemId.split("-")[1]);
+
+    const isInvalidDrop =
+      (destination.droppableId === "col3" && source.droppableId === "col1") || // 첫 번째에서 세 번째로 이동 불가
+      (destination.index > 0 &&
+        columns[destination.droppableId].items[
+          destination.index - 1
+        ].content.split(" ")[1] %
+          2 ===
+          0 &&
+        draggingItemK % 2 === 0 &&
+        destination.index !== 0); // 짝수 아이템이 짝수 아이템 앞에 올 수 없음
+
+    setInvalidDrop(isInvalidDrop);
+  };
+
   const handleDragEnd = (result) => {
+    setDraggingItemId(null);
+    setInvalidDrop(false);
     if (!result.destination) return;
+
     const { source, destination } = result;
+
+    const draggingItemK = parseInt(draggingItemId.split("-")[1]);
+
+    const isInvalidDrop =
+      (destination.droppableId === "col3" && source.droppableId === "col1") || // 첫 번째에서 세 번째로 이동 불가
+      (destination.index > 0 &&
+        columns[destination.droppableId].items[
+          destination.index - 1
+        ].content.split(" ")[1] %
+          2 ===
+          0 &&
+        draggingItemK % 2 === 0 &&
+        destination.index !== 0); // 짝수 아이템이 짝수 아이템 앞에 올 수 없음
+
+    if (isInvalidDrop) {
+      setInvalidDrop(isInvalidDrop);
+      toast.error("올바르게 이동해주세요.");
+      return;
+    }
 
     if (source.droppableId === destination.droppableId) {
       const column = columns[source.droppableId];
@@ -54,17 +107,45 @@ export default function App() {
         },
       }));
     }
+    toast.success(
+      `${source.droppableId} -> ${destination.droppableId} 이동 성공!`
+    );
   };
-
   return (
-    <div className="flex justify-around items-center h-screen overflow-y-scroll">
-      <DragDropContext onDragEnd={handleDragEnd}>
-        {Object.values(columns).map((column) => (
-          <div className="h-screen overflow-y-scroll w-full flex flex-col justify-center items-center">
-            <Column key={column.id} items={column.items} columnId={column.id} />
-          </div>
-        ))}
-      </DragDropContext>
+    <div>
+      <div className="pl-2 pt-2">
+        <div className="flex items-center">
+          <p className="size-4 bg-red-900" /> &nbsp;: 제약사항 있음
+        </div>
+        <div className="flex items-center">
+          <p className="size-4 bg-blue-600" />
+          &nbsp;: 제약사항 없음
+        </div>
+      </div>
+      <div className="flex justify-around items-center h-screen ">
+        <Toaster />
+        <DragDropContext
+          onDragEnd={handleDragEnd}
+          onDragStart={handleDragStart}
+          onDragUpdate={handleDragUpdate}
+        >
+          {Object.values(columns).map((column) => (
+            <div
+              className="h-screen overflow-y-scroll w-full flex flex-col justify-center items-center"
+              key={column.id}
+            >
+              <Column
+                items={column.items.map((item) => ({
+                  ...item,
+                  isDragging: item.id === draggingItemId,
+                  isInvalidDrop: invalidDrop,
+                }))}
+                columnId={column.id}
+              />
+            </div>
+          ))}
+        </DragDropContext>
+      </div>
     </div>
   );
 }
--- src/components/ColumnItem.jsx
+++ src/components/ColumnItem.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Draggable } from "react-beautiful-dnd";
 
 const ColumnItem = React.memo(({ item, index }) => {
+  console.log(item.isDragging);
   return (
     <Draggable draggableId={item.id} index={index}>
       {(provided, snapshot) => (
@@ -9,10 +10,12 @@ const ColumnItem = React.memo(({ item, index }) => {
           ref={provided.innerRef}
           {...provided.draggableProps}
           {...provided.dragHandleProps}
-          className={`select-none p-4 mb-2  ${
-            snapshot.isDragging ? "bg-red-400" : "bg-gray-400"
-          }  transition-all rounded-2xl`}
-          onClick={() => console.log(provided.draggableProps.style)}
+          className={`select-none p-4 mb-2  
+            ${
+              snapshot.isDragging ? "bg-blue-600" : "bg-gray-400"
+            }  transition-all rounded-2xl  ${
+            item.isDragging && item.isInvalidDrop ? "bg-red-900" : ""
+          }`}
         >
           {item.content}
         </div>
